ef9c948bad68064c4d146a64a67ae5370e5349fd
pijjacheese <97718478+pijjacheese@users.noreply.github.com>
2022-02-20 23:08:17 +0800

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,7 +1,7 @@
 import 'dart:io' show Platform;
 
 class Secret {
-  static const ANDROID_CLIENT_ID = "201133308756-mcmfbno9cuhb80s2moaahdfhfqap1fkv.apps.googleusercontent.com";
+  static const ANDROID_CLIENT_ID = "";
   static String getId() => Platform.isAndroid ? Secret.ANDROID_CLIENT_ID : Secret.ANDROID_CLIENT_ID ;
 
 }
