8c532981a0d4726692d0f1f91fe0d958995c9977
pijjacheese <97718478+pijjacheese@users.noreply.github.com>
2022-02-20 22:48:05 +0800

    Delete lib/calender directory

--- lib/calender/secrets.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io' show Platform;
-
-class Secret {
-  static const ANDROID_CLIENT_ID = "";
-  static String getId() => Platform.isAndroid ? Secret.ANDROID_CLIENT_ID : Secret.ANDROID_CLIENT_ID ;
-
-}
